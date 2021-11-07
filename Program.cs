af94bbf81bb2030ca7bc90c775487e248a6b619f
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-07 10:34:57 +0100

    Zrobione zadanie E03

--- Program.cs
+++ Program.cs
@@ -2,11 +2,36 @@
 
 namespace sda_csharp_exercises
 {
+
+
     class Program
     {
         static void Main(string[] args)
+        // E03
+        //Napisz funkcję tworzącą ciąg arytmetyczny o podanych: długości, pierwszym elemencie, różnicy ciągu.
         {
-            
+            CiagArytmetyczny();
+      
         }
+
+        public static void CiagArytmetyczny() 
+        {
+
+            Console.WriteLine("Wpis długość ciągu arytmetycznego");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz pierwszy element ciągu");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz różnicę ciągu");
+            int element = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ciąg arytmetyczny =>");
+            Console.Write($" {first}");
+            for (int i = 1; i < lenght; i++)
+            {
+                Console.Write($" {first + (i * element)}");
+            }
+        }
+    
+    
     }
+
 }
