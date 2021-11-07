e64766bf2f5be356c82bf07c864bbf957b72739f
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-07 11:07:14 +0100

    Zrobione zadanie E04

--- Program.cs
+++ Program.cs
@@ -2,11 +2,46 @@
 
 namespace sda_csharp_exercises
 {
+    /*
+    Namaluj trójkąt z gwiazdek o zadanej liczbie linii.Dopełnij pod spodem odwróconym trójkątem tak
+    jak w przykładzie poniżej.Zwróć uwagę, że linia z maksymalną liczba gwiazdek jest tylko jedna.
+    Np.dla 3 linii:
+    *
+    **
+    ***
+    **
+    **/
+
     class Program
     {
+        public static void PrintTriangle(int width, char character)
+        {
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine("");
+            }
+            for (int i = width - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine("");
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj szerokość podstawy: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj znak do budowy trójkąta: ");
+            char character = Convert.ToChar(Console.ReadLine());
+
+            PrintTriangle(width, character);
         }
     }
 }
