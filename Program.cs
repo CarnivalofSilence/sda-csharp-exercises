3d89d43004c82710b8bcf9357cedb5ba862c9469
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-07 13:02:18 +0100

    Wykonanie zadania E06

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace sda_csharp_exercises
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
+
+/*    E06
+    Dla podanego ciągu znaków oznaczającego hasło, sprawdź, czy spełnia on następujące warunki
+    złożoności hasła:
+    • długość co najmniej 6 znaków
+    • występują co najmniej jedna duża i jedna mała litera
+    • występuje co najmniej jedna cyfra
+    • występuje co najmniej jeden znak specjalny spośród następujących: &*$#*/
+    
+            public static bool passwordChecker(string password)
+            {
+                return Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[&*$#]") && password.Length >= 6;
+
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine(passwordChecker("Lubie$"));
+                Console.WriteLine(passwordChecker("a"));
+                Console.WriteLine(passwordChecker("Lubie dolary"));
+                Console.WriteLine(passwordChecker("lubie male litery"));
+                Console.WriteLine(passwordChecker("JESTEM ZLY"));
+            }
         }
     }
-}
+
+    
+
