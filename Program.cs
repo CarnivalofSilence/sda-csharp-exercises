470fd0384f4321556a71d4d4e59f5fe8ae190f29
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-20 14:08:14 +0100

    Ovverriding/toString

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            
+            /*  Person person1 = new Person("Joanna", "Kowalska", 1982);
+              person1.WhoAmI();
+              Person person2 = new Employee("Adam", "Nowak", 2000, 6000);
+              person2.WhoAmI();
+              Employee employee = new Employee("Jan", "Kowalski", 1981, 5000);
+              employee.WhoAmI();
+  */
+            List<Person> people = new List<Person>();
+            people.Add(new Person("Jan", "Kowalski", 1981));
+            people.Add(new Employee("Adam", "Nowak", 2000, 5000));
+
+            foreach (var p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+
+
         }
     }
 }
+
