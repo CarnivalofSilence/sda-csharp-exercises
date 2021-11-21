20d6d6518d33b9085489df913818adcc0d779c93
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-21 13:53:19 +0100

    Ready to use

---
+++ LineSegment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+	class LineSegment
+	{
+		public MyPoint A { get; set; }
+		public MyPoint B { get; set; }
+
+
+
+		public LineSegment(MyPoint a, MyPoint b)
+		{
+			A = a;
+			B = b;
+		}
+
+		public LineSegment(double ax, double ay, double az, double bx, double by, double bz)
+		{
+			A = new MyPoint3D(ax, ay, az);
+			B = new MyPoint3D(bx, by, bz);
+		}
+
+		public LineSegment(double ax, double ay, double bx, double by)
+		{
+			A = new MyPoint(ax, ay);
+			B = new MyPoint(bx, by);
+		}
+
+		public double Lenght()
+		{
+			return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+		}
+
+		public double Lenght3D()
+		{
+			MyPoint3D A3D = (MyPoint3D)A;
+			MyPoint3D B3D = (MyPoint3D)B;
+			return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2) + Math.Pow(B3D.Z - A3D.Z, 2));
+		}
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,28 @@ namespace sda_csharp_exercises
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
-}
+			static void Main(string[] args)
+			{
+				MyPoint myPoint1 = new MyPoint(2, 3);
+				MyPoint myPoint2 = new MyPoint(5, 2);
+
+				MyPoint3D myPoint3 = new MyPoint3D(2, 3, 5);
+				MyPoint3D myPoint4 = new MyPoint3D(5, 2, 1);
+
+				LineSegment lineSegment1 = new LineSegment(myPoint1, myPoint2);
+				LineSegment lineSegment2 = new LineSegment(2, 3, 5, 2);
+
+				Console.WriteLine(lineSegment1.Lenght());
+
+				Console.WriteLine(lineSegment2.Lenght());
+
+				LineSegment lineSegment3 = new LineSegment(myPoint3, myPoint4);
+				LineSegment lineSegment4 = new LineSegment(2, 3, 5, 5, 2, 1);
+
+				Console.WriteLine(lineSegment3.Lenght3D());
+
+				Console.WriteLine(lineSegment4.Lenght3D());
+
+			}
+		}
+	}
