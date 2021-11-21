c4df8a8db18b7224021414fcec403970c5db6bb3
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-21 11:49:21 +0100

    Abstract

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
@@ -6,7 +7,25 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            
+            Circle circle = new Circle(3.4);
+            Square square = new Square(3.4);
+
+            List<Shape> list = new List<Shape>();
+
+            //Shape shapes = new Shape();
+            list.Add(circle);
+            list.Add(square);
+
+            Console.WriteLine("CIRCLE: " + circle.GetArea());
+            Console.WriteLine("SQUARE " + square.GetArea());
+
+            //double total = 0;
+            //foreach (Shape shape in list)
+            //{
+            //    total += shape.GetArea();
+            //}
+
+            Console.WriteLine($"TOTAL: " + Shape.GetTotalArea(list));
         }
     }
 }
---
+++ Shape.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    abstract class Shape
+    {
+        //private List<Shape> list = new List<Shape>();
+
+        //public void Add(Shape shape)
+        //{
+        //    list.Add(shape);
+        //}
+
+        public abstract double GetArea();
+        //{
+        //    double total = 0;
+        //    foreach (Shape shape in list)
+        //    {
+        //        total += shape.GetArea();
+        //    }
+        //    return total;
+        //}
+
+        public static double GetTotalArea(List<Shape> list)
+        {
+            double total = 0;
+            foreach (Shape shape in list)
+            {
+                total += shape.GetArea();
+            }
+            return total;
+        }
+    }
+}
