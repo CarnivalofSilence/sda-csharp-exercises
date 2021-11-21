2b2dffed2948a7536937dc389e3dc3f8ff003fc6
CarnivalofSilence <49090920+CarnivalofSilence@users.noreply.github.com>
2021-11-21 12:16:20 +0100

    Player Example

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            
+            Player player = new Player();
+            player.Name = "Conan";
+            /* player.Stats = new PlayerStats();
+             player.Stats.XP = 60;*/
+            player.XP = 60;
+            Item sword = new Item();
+            sword.Name = "Exalibur";
+            player.Items.Add(sword);
+            /*  Console.WriteLine(player.Stats.XP);*/
+            Console.WriteLine(player.XP);
+            foreach (var item in player.Items)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
     }
 }
